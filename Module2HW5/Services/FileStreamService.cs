dc6eaf21551ef9b4d2cf26de2aa3f150c3fe6bef
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2021-11-24 21:05:50 +0200

    Add LoggerService, ActionsService and FileService implementation.

---
+++ Module2HW5/Services/FileStreamService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Module2HW5.Helpers;
+
+namespace Module2HW5.Services
+{
+    public class FileStreamService : IDisposable
+    {
+        private readonly FileStream _stream = null;
+
+        public FileStreamService(string filename, FileMode mode)
+        {
+            try
+            {
+                _stream = new FileStream(filename, mode);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"The process failed: {e}");
+            }
+        }
+
+        public bool WriteLine(string text)
+        {
+            if (_stream.CanWrite)
+            {
+                StreamHelper.AddText(_stream, $"\n{text}");
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Dispose()
+        {
+            if (_stream != null)
+            {
+                _stream.Dispose();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
